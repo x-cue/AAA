7ca2b758e66abb682fcd723df7d43125e44f900b
pack0 <brandonpack03@gmail.com>
2024-08-09 23:27:53 -0400

    a new beginning

--- src/client/java/com/xcue/mods/notafk/NotAfkMod.java
+++ src/client/java/com/xcue/mods/notafk/NotAfkMod.java
@@ -6,19 +6,36 @@ import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class NotAfkMod implements ClientModInitializer {
     boolean autoClickEnabled = false;
     int ticks = 0;
+
+    public boolean getCurrItem(ClientPlayerEntity player){
+        Item currItem = player.getMainHandStack().getItem();
+        Pattern allowedItems = Pattern.compile("^.*Sword$");
+
+        Matcher matcher = allowedItems.matcher(currItem.getName().getString());
+
+        return matcher.matches();
+    }
     @Override
     public void onInitializeClient() {
 
 
         AttackEntityCallback.EVENT.register(((player1, world, hand, entity, hitResult) -> {
-            if (!entity.isPlayer() && !autoClickEnabled){
+            // Is sword
+
+            if (!entity.isPlayer() && !autoClickEnabled && getCurrItem((ClientPlayerEntity)player1)){
                 ticks = 0;
                 autoClickEnabled = true;
             }
@@ -33,7 +50,7 @@ public class NotAfkMod implements ClientModInitializer {
 
             if(autoClickEnabled && (ticks % 2 == 0)){
                 HitResult hRes = client.crosshairTarget;
-                if(client.mouse.wasLeftButtonClicked() && ticks != 0){
+                if(client.mouse.wasLeftButtonClicked() && ticks != 0 || !getCurrItem(player)){
                     autoClickEnabled = false;
                     return;
                 }
