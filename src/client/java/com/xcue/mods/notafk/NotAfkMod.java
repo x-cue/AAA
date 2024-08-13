d6b962d277f45e611e72be06e93a3438ea68078a
x-cue <174210264+x-cue@users.noreply.github.com>
2024-08-12 19:14:08 -0500

    Rebase

--- src/client/java/com/xcue/mods/notafk/NotAfkMod.java
+++ src/client/java/com/xcue/mods/notafk/NotAfkMod.java
@@ -3,6 +3,7 @@ package com.xcue.mods.notafk;
 import com.xcue.lib.AAAMod;
 import com.xcue.lib.events.chat.PlayerMessageReceivedCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -19,14 +20,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-/**
- * Mob AutoClicker
- */
+
 public class NotAfkMod implements AAAMod {
     boolean autoClickEnabled = false;
     int ticks = 0;
-
-    public boolean getCurrItem(ClientPlayerEntity player) {
+    int xLoc;
+    int yLoc;
+    int zLoc;
+    public boolean getCurrItem(ClientPlayerEntity player){
         Item currItem = player.getMainHandStack().getItem();
         Pattern allowedItems = Pattern.compile("^.*Sword$");
 
@@ -34,12 +35,11 @@ public class NotAfkMod implements AAAMod {
 
         return matcher.matches();
     }
-
-    public boolean getCurrItemHealth(ClientPlayerEntity player, Hand hand) {
+    public boolean getCurrItemHealth(ClientPlayerEntity player, Hand hand){
         ItemStack currItem = player.getStackInHand(hand);
-        return currItem.getMaxDamage() - currItem.getDamage() < 20;
+        int i = currItem.getMaxDamage() - currItem.getDamage();
+        return(i < 20);
     }
-
     @Override
     public void init() {
         PlayerMessageReceivedCallback.EVENT.register(((player, msg) -> {
@@ -47,16 +47,24 @@ public class NotAfkMod implements AAAMod {
             player.sendMessage(Text.literal("Disabled AutoClicker"));
         }));
 
-        AttackEntityCallback.EVENT.register(((player1, world, hand, entity, hitResult) -> {
+        AttackEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> {
             // Is sword
-            ClientPlayerEntity player = MinecraftClient.getInstance().player;
-            assert player != null;
-
-            if (!entity.isPlayer() && !autoClickEnabled && getCurrItem(player)){
+            ClientPlayerEntity player1 = MinecraftClient.getInstance().player;
+            if (!entity.isPlayer() && !autoClickEnabled && getCurrItem(player1)){
                 ticks = 0;
-                if (getCurrItemHealth(player, hand)) {
-                    player.networkHandler.sendChatCommand("/fix");
-                    if (!getCurrItemHealth(player, hand)) {
+                autoClickEnabled = true;
+                xLoc = player1.getBlockX();
+                yLoc = player1.getBlockY();
+                zLoc = player1.getBlockZ();
+            }
+            if(getCurrItemHealth(player1,hand) && getCurrItem(player1)){
+                player1.networkHandler.sendChatCommand("fix");
+                if(!getCurrItemHealth(player1,hand)){
+                    autoClickEnabled = true;
+                    return ActionResult.PASS;
+                } else {
+                    player1.getInventory().scrollInHotbar(-1);
+                    if(getCurrItem(player1)){
                         autoClickEnabled = true;
                         return ActionResult.PASS;
                     } else {
@@ -64,20 +72,26 @@ public class NotAfkMod implements AAAMod {
                         return ActionResult.FAIL;
                     }
                 }
-                autoClickEnabled = true;
             }
-            return ActionResult.SUCCESS;
+            if(player1.getBlockX() != xLoc || player1.getBlockY() != yLoc || player1.getBlockZ() != zLoc){
+                autoClickEnabled = false;
+                return ActionResult.FAIL;
+            }
+            return ActionResult.PASS;
         }));
 
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            autoClickEnabled = false;
+        });
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             ClientPlayerEntity player = MinecraftClient.getInstance().player;
-            if (player == null) {
+            if(player == null){
                 return;
             }
-            if (autoClickEnabled && (ticks % 2 == 0)) {
+            if(autoClickEnabled && (ticks % 2 == 0)){
                 HitResult hRes = client.crosshairTarget;
 
-                if (client.mouse.wasLeftButtonClicked() && ticks != 0 || !getCurrItem(player)) {
+                if(client.mouse.wasLeftButtonClicked() && ticks != 0 || !getCurrItem(player)){
                     autoClickEnabled = false;
                     return;
                 }
